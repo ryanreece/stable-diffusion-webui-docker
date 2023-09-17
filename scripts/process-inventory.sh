d0bdeebdacb01a49930d7bd00684cfc6f5fde107
Ryan Reece <ryan@reece.ai>
2023-09-17 08:29:55 -0700

    Fix order of commands for process-inventory.sh

--- scripts/process-inventory.sh
+++ scripts/process-inventory.sh
@@ -3,14 +3,14 @@
 echo "Sourcing .env file"
 source .env
 
+echo "Syncing ../output/saved/ with s3://$BUCKET_NAME/images/"
+s3cmd sync --skip-existing --delete-removed ../output/saved/ s3://$BUCKET_NAME/images/
+
 echo "Creating inventory.json using csvToJson.js"
 node csvToJson.js
 
 echo "Putting inventory.json in s3://$BUCKET_NAME/images/ using s3cmd"
 s3cmd put inventory.json s3://$BUCKET_NAME/images/
-
-echo "Syncing ../output/saved/ with s3://$BUCKET_NAME/images/"
-s3cmd sync --skip-existing --delete-removed ../output/saved/ s3://$BUCKET_NAME/images/
 
 echo "Setting acls to public for s3://$BUCKET_NAME/images/"
 s3cmd setacl s3://$BUCKET_NAME/images/ --acl-public --recursive
