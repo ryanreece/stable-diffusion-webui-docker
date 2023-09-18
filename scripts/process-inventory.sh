d5532a47716115c5eadb95bdbe999d72c8571ba0
Ryan Reece <ryan@reece.ai>
2023-09-18 14:55:26 -0700

    Update process-inventory.sh to set acl-public during ops

--- scripts/process-inventory.sh
+++ scripts/process-inventory.sh
@@ -4,13 +4,10 @@ echo "Sourcing .env file"
 source .env
 
 echo "Syncing ../output/saved/ with s3://$BUCKET_NAME/images/"
-s3cmd sync --skip-existing --delete-removed ../output/saved/ s3://$BUCKET_NAME/images/
+s3cmd sync --acl-public --skip-existing --delete-removed ../output/saved/ s3://$BUCKET_NAME/images/
 
 echo "Creating inventory.json using csvToJson.js"
 node csvToJson.js
 
 echo "Putting inventory.json in s3://$BUCKET_NAME/images/ using s3cmd"
-s3cmd put inventory.json s3://$BUCKET_NAME/images/
-
-echo "Setting acls to public for s3://$BUCKET_NAME/images/"
-s3cmd setacl s3://$BUCKET_NAME/images/ --acl-public --recursive
+s3cmd put --acl-public inventory.json s3://$BUCKET_NAME/images/
